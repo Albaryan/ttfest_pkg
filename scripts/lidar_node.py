1bb47fe72a65cbe82e8f28830db6638599b6f4c0
Albaryan <alper358tuna@gmail.com>
2024-05-15 12:31:38 +0300

    fix build errors and add lidar node

---
+++ scripts/lidar_node.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import LaserScan
+
+class CameraNode(Node):
+    def __init__(self):
+        super().__init__("camera_node")
+        self.subscriber_ = self.create_subscription(LaserScan, "/lidar/data", self.callback_lidar, 10)
+                
+        self.get_logger().info("Lidar node has started.")
+
+    def callback_lidar(self, msg):
+        
+        print(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CameraNode()
+    rclpy.spin(node)
+    rclpy.shutdown
+
+if __name__=="__main__":
+    main()
